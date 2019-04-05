48dd1045f43736fcaf7758656098d699c1cb9298
Tino (abandoned profile) <tino@tokic.com.hr>
2019-04-05 12:15:19 +0300

    Prep for prod

--- src/index.js
+++ src/index.js
@@ -24,11 +24,11 @@ let dbInstance;
 let app = express();
 
 const getUser = id =>
-  dbInstance("wp_users")
+    dbInstance("wp_users")
     .leftJoin("wp_pms_member_subscriptions", "user_id", "user_id")
     .where({
-      user_id: id,
-      status: "active"
+        user_id: id,
+        status: "active"
     })
     .select("user_nicename", "status", "subscription_plan_id", "user_email")
     .first();
@@ -38,167 +38,166 @@ app.use(CookieParser());
 
 // 3rd party middleware
 app.use(
-  cors({
-    exposedHeaders: config.corsHeaders
-  })
+    cors({
+        exposedHeaders: config.corsHeaders
+    })
 );
 app.use(helmet());
 app.use(bodyParser.json({ type: "application/json" }));
 
 // connect to db
 initializeDb(db => {
-  // internal middleware
-  dbInstance = db;
+    // internal middleware
+    dbInstance = db;
 
-  app.use(middleware({ config, db }));
-  app.use(
-    JwtValidator({
-      secret: config.jwtKey,
-      getToken: function fromHeaderOrQuerystring(req) {
-        if (
-          req.headers.authorization &&
-          req.headers.authorization.split(" ")[0] === "Bearer"
-        ) {
-          return req.headers.authorization.split(" ")[1];
-        } else if (req.cookies["access_token"]) {
-          return req.cookies["access_token"];
-        }
-        return null;
-      }
-    }).unless(function(req) {
-      return req.path.includes(["/api/saveGif"]);
-    })
-  );
-  // api router
-  app.use("/api", api({ config, db }));
+    app.use(middleware({ config, db }));
+    app.use(
+        JwtValidator({
+            secret: config.jwtKey,
+            getToken: function fromHeaderOrQuerystring(req) {
+                if (
+                    req.headers.authorization &&
+                    req.headers.authorization.split(" ")[0] === "Bearer"
+                ) {
+                    return req.headers.authorization.split(" ")[1];
+                } else if (req.cookies["access_token"]) {
+                    return req.cookies["access_token"];
+                }
+                return null;
+            }
+        }).unless(function(req) {
+            return req.path.includes(["/api/saveGif"]);
+        })
+    );
+    // api router
+    app.use("/api", api({ config, db }));
 
-  app.server.listen(process.env.PORT || config.port, () => {
-    console.log(
-      `Started on IP: ${app.server.address().address} port ${
+    app.server.listen(process.env.PORT || config.port, () => {
+        console.log(
+            `Started on IP: ${app.server.address().address} port ${
         app.server.address().port
       }`
-    );
-  });
+        );
+    });
 });
 
 const removeAndSaveQueue = params => {
-  console.log("Removing");
-  const index = global.queue.findIndex(
-    item =>
-      item.user === params.user &&
-      item.group === params.group &&
-      item.hash === params.hash
-  );
-  global.queue.splice(index, 1);
+    console.log("Removing");
+    const index = global.queue.findIndex(
+        item =>
+        item.user === params.user &&
+        item.group === params.group &&
+        item.hash === params.hash
+    );
+    global.queue.splice(index, 1);
 
-  fs.writeFile("./queue.json", JSON.stringify(queue), function(err) {
-    if (err) {
-      console.log("err saving queue", err);
-    } else {
-      console.log("Queue updated");
-    }
-  });
+    fs.writeFile("./queue.json", JSON.stringify(queue), function(err) {
+        if (err) {
+            console.log("err saving queue", err);
+        } else {
+            console.log("Queue updated");
+        }
+    });
 };
 
-global.renderQueue = new Queue(async (params, callback) => {
-  try {
-    const user = await getUser(params.user);
-    if (!params.userTitle || params.userTitle == "") {
-      params.userTitle = moment().format(MYSQL_TIME_FORMAT);
+global.renderQueue = new Queue(async(params, callback) => {
+    try {
+        const user = await getUser(params.user);
+        if (!params.userTitle || params.userTitle == "") {
+            params.userTitle = moment().format(MYSQL_TIME_FORMAT);
+        }
+        params.hash = params.userTitle ? md5(params.userTitle) : params.hash;
+        console.log("Starting " + user.user_nicename + "/" + params.hash);
+        browser = await puppeteer.launch({
+            headless: true
+        });
+        params.renderStartTime = moment();
+
+        page = await browser.newPage();
+        const override = Object.assign(page.viewport(), { width: 800 });
+        await page.setViewport(override);
+        await page.goto(
+            config.hostname + "/?group=" +
+            params.group +
+            "&hash=" +
+            params.hash +
+            "&startAt=0&endAt=10&puppeteer=true&username=" +
+            user.user_nicename, {
+                timeout: 0,
+                waitUntil: "networkidle2"
+            }
+        );
+
+        await page.waitFor("input[name=userdata]");
+        await page.$eval(
+            "input[name=userdata]",
+            (el, value) => (el.value = value),
+            JSON.stringify({...params, ...user })
+        );
+
+        var functionToInject = function(data) {
+            return {
+                done: document.encodingDone,
+                renderProgress: document.renderProgress,
+                captureProgress: document.capturedFramesProgress,
+                framesPerSecond: document.framesPerSecond,
+                msg: document.msg
+            };
+        };
+
+        const checkInterval = setInterval(async db => {
+            var data = await page.evaluate(functionToInject);
+
+            if (data.done) {
+                clearInterval(checkInterval);
+
+                sendMail(
+                    user.user_email,
+                    "Your chart is ready",
+                    "You can get your gif at: " +
+                    config.apiHostname +
+                    "/api/downloadUserGif/" +
+                    params.hash +
+                    ".gif\r\n" +
+                    "You can get your video at: " +
+                    config.apiHostname +
+                    "/api/downloadUserVideo/" +
+                    params.hash +
+                    ".mp4"
+                );
+
+                addToStats(data.framesPerSecond);
+
+                updateChartLog(params, dbInstance);
+
+                browser.close();
+
+                const filePath =
+                    __dirname +
+                    "/../data/" +
+                    user.user_nicename +
+                    "/" +
+                    md5(params.userTitle);
+
+                await exec(
+                    "ffmpeg -i " +
+                    filePath +
+                    '.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -b 5000k -s 1920x1080 ' +
+                    filePath +
+                    ".mp4"
+                );
+                removeAndSaveQueue(params);
+
+                callback(null, null);
+            }
+        }, 1000);
+    } catch (e) {
+        console.log(e);
     }
-    params.hash = params.userTitle ? md5(params.userTitle) : params.hash;
-    console.log("Starting " + user.user_nicename + "/" + params.hash);
-    browser = await puppeteer.launch({
-      headless: true
-    });
-    params.renderStartTime = moment();
-
-    page = await browser.newPage();
-    const override = Object.assign(page.viewport(), { width: 800 });
-    await page.setViewport(override);
-    await page.goto(
-      "http://app.chartlapse.lo:3000/?group=" +
-        params.group +
-        "&hash=" +
-        params.hash +
-        "&startAt=0&endAt=10&puppeteer=true&username=" +
-        user.user_nicename,
-      {
-        timeout: 0,
-        waitUntil: "networkidle2"
-      }
-    );
-
-    await page.waitFor("input[name=userdata]");
-    await page.$eval(
-      "input[name=userdata]",
-      (el, value) => (el.value = value),
-      JSON.stringify({ ...params, ...user })
-    );
-
-    var functionToInject = function(data) {
-      return {
-        done: document.encodingDone,
-        renderProgress: document.renderProgress,
-        captureProgress: document.capturedFramesProgress,
-        framesPerSecond: document.framesPerSecond,
-        msg: document.msg
-      };
-    };
-
-    const checkInterval = setInterval(async db => {
-      var data = await page.evaluate(functionToInject);
-
-      if (data.done) {
-        clearInterval(checkInterval);
-
-        sendMail(
-          user.user_email,
-          "Your chart is ready",
-          "You can get your gif at: " +
-            config.apiHostname +
-            "/api/downloadUserGif/" +
-            params.hash +
-            ".gif\r\n" +
-            "You can get your video at: " +
-            config.apiHostname +
-            "/api/downloadUserVideo/" +
-            params.hash +
-            ".mp4"
-        );
-
-        addToStats(data.framesPerSecond);
-
-        updateChartLog(params, dbInstance);
-
-        browser.close();
-
-        const filePath =
-          __dirname +
-          "/../data/" +
-          user.user_nicename +
-          "/" +
-          md5(params.userTitle);
-
-        await exec(
-          "ffmpeg -i " +
-            filePath +
-            '.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -b 5000k -s 1920x1080 ' +
-            filePath +
-            ".mp4"
-        );
-        removeAndSaveQueue(params);
-
-        callback(null, null);
-      }
-    }, 1000);
-  } catch (e) {
-    console.log(e);
-  }
 }, 1);
 
 global.renderQueue.drain = function() {
-  console.log("all items have been processed");
+    console.log("all items have been processed");
 };
 
 /**
@@ -207,9 +206,9 @@ global.renderQueue.drain = function() {
  */
 global.queue = fs.existsSync("./queue.json");
 if (!global.queue) {
-  global.queue = [];
+    global.queue = [];
 } else {
-  global.queue = JSON.parse(fs.readFileSync("./queue.json"));
+    global.queue = JSON.parse(fs.readFileSync("./queue.json"));
 }
 
 /**
@@ -218,32 +217,32 @@ if (!global.queue) {
  */
 global.renderStats = fs.existsSync("./renderStats.json");
 if (!global.renderStats) {
-  global.renderStats = [];
+    global.renderStats = [];
 } else {
-  global.renderStats = JSON.parse(fs.readFileSync("./renderStats.json"));
+    global.renderStats = JSON.parse(fs.readFileSync("./renderStats.json"));
 }
 
 global.queue.forEach(item => {
-  global.renderQueue.push(item, null, true);
+    global.renderQueue.push(item, null, true);
 });
 
 const addToStats = time => {
-  global.renderStats.push(time);
+    global.renderStats.push(time);
 
-  if (global.renderStats.length > 100) {
-    global.renderStats.slice(0, 1);
-  }
-  fs.writeFile(
-    "./renderStats.json",
-    JSON.stringify(global.renderStats),
-    function(err) {
-      if (err) {
-        console.log("err saving stats", err);
-      } else {
-        console.log("Stats updated");
-      }
+    if (global.renderStats.length > 100) {
+        global.renderStats.slice(0, 1);
     }
-  );
+    fs.writeFile(
+        "./renderStats.json",
+        JSON.stringify(global.renderStats),
+        function(err) {
+            if (err) {
+                console.log("err saving stats", err);
+            } else {
+                console.log("Stats updated");
+            }
+        }
+    );
 };
 
 export default app;
--- src/lib/util.js
+++ src/lib/util.js
@@ -2,11 +2,13 @@ import nodemailer from "nodemailer";
 import moment from "moment";
 import md5 from "md5";
 import {
-  FREE_SUBSCRIPTION_PLAN_ID,
-  BASIC_SUBSCRIPTION_PLAN_ID,
-  PRO_SUBSCRIPTION_PLAN_ID,
-  MYSQL_TIME_FORMAT
+    FREE_SUBSCRIPTION_PLAN_ID,
+    BASIC_SUBSCRIPTION_PLAN_ID,
+    PRO_SUBSCRIPTION_PLAN_ID,
+    MYSQL_TIME_FORMAT
 } from "../api/constants";
+const isWin = process.platform === "win32";
+
 /**	Creates a callback that proxies node callback style arguments to an Express Response object.
  *	@param {express.Response} res	Express HTTP Response
  *	@param {number} [status=200]	Status code to send on success
@@ -18,112 +20,118 @@ import {
  */
 
 export function toRes(res, status = 200) {
-  return (err, thing) => {
-    if (err) return res.status(500).send(err);
+    return (err, thing) => {
+        if (err) return res.status(500).send(err);
 
-    if (thing && typeof thing.toObject === "function") {
-      thing = thing.toObject();
-    }
-    res.status(status).json(thing);
-  };
+        if (thing && typeof thing.toObject === "function") {
+            thing = thing.toObject();
+        }
+        res.status(status).json(thing);
+    };
 }
 
-const transporter = nodemailer.createTransport({
-  sendmail: true,
-  newline: "unix",
-  path: "/usr/sbin/sendmail"
-});
+const opts = isWin ? {
+    host: "smtp.zoho.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+        user: "info@lorecore.com", // generated ethereal user
+        pass: "theborg8472" // generated ethereal password
+    }
+} : {
+    sendmail: true,
+    newline: "unix",
+    path: "/usr/sbin/sendmail"
+};
+
+const transporter = nodemailer.createTransport(opts);
 
 export function logChartCreation(params, db) {
-  const unique_id = md5(params.hash + params.user + moment().valueOf());
-  db("graphs")
-    .insert({
-      unique_id: unique_id,
-      user_id: params.user * 1,
-      graph_title: params.userTitle,
-      graph_data: JSON.stringify(params.userData),
-      request_time: moment().format(MYSQL_TIME_FORMAT),
-      graph_hash: params.hash,
-      estimated_render_time: params.timeToProcess,
-      queue_position: params.queuePosition
-    })
-    .then(res => console.log("Chart logged", res));
+    const unique_id = md5(params.hash + params.user + moment().valueOf());
+    db("graphs")
+        .insert({
+            unique_id: unique_id,
+            user_id: params.user * 1,
+            graph_title: params.userTitle,
+            graph_data: JSON.stringify(params.userData),
+            request_time: moment().format(MYSQL_TIME_FORMAT),
+            graph_hash: params.hash,
+            estimated_render_time: params.timeToProcess,
+            queue_position: params.queuePosition
+        })
+        .then(res => console.log("Chart logged", res));
 
-  return unique_id;
+    return unique_id;
 }
 
 export async function isUserAllowedToCreate(params, db) {
-  const plan = await db("wp_pms_member_subscriptions")
-    .where({ user_id: params.user })
-    .select("subscription_plan_id as plan_id")
-    .first();
+    const plan = await db("wp_pms_member_subscriptions")
+        .where({ user_id: params.user })
+        .select("subscription_plan_id as plan_id")
+        .first();
 
-  const number = await db("graphs as g")
-    .leftJoin(
-      "wp_pms_member_subscriptions as subs",
-      "subs.user_id",
-      "g.user_id"
-    )
-    .where({
-      "g.user_id": params.user,
-      "subs.status": "active"
-    })
+    const number = await db("graphs as g")
+        .leftJoin(
+            "wp_pms_member_subscriptions as subs",
+            "subs.user_id",
+            "g.user_id"
+        )
+        .where({
+            "g.user_id": params.user,
+            "subs.status": "active"
+        })
 
     .whereNotNull("render_time")
-    .whereRaw("DATE(render_finish_time ) = CURDATE()")
-    .count("g.id as nr")
-    .first();
+        .whereRaw("DATE(render_finish_time ) = CURDATE()")
+        .count("g.id as nr")
+        .first();
 
-  let left;
+    let left;
 
-  switch (plan.plan_id) {
-    case FREE_SUBSCRIPTION_PLAN_ID:
-      left = 0;
-      break;
-    case BASIC_SUBSCRIPTION_PLAN_ID:
-      left = 1 - number.nr;
-      break;
-    case PRO_SUBSCRIPTION_PLAN_ID:
-      left = 10 - number.nr;
-      break;
-  }
+    switch (plan.plan_id) {
+        case FREE_SUBSCRIPTION_PLAN_ID:
+            left = 0;
+            break;
+        case BASIC_SUBSCRIPTION_PLAN_ID:
+            left = 1 - number.nr;
+            break;
+        case PRO_SUBSCRIPTION_PLAN_ID:
+            left = 10 - number.nr;
+            break;
+    }
 
-  return { left };
+    return { left };
 }
 
 export function updateChartLog(params, db) {
-  const finishTime = moment();
+    const finishTime = moment();
 
-  const duration = moment.duration(finishTime.diff(params.renderStartTime));
+    const duration = moment.duration(finishTime.diff(params.renderStartTime));
 
-  db("graphs")
-    .where(
-      { user_id: params.user * 1, unique_id: params.unique_id },
-      {
-        render_finish_time: finishTime.format(MYSQL_TIME_FORMAT),
-        render_time: duration.asMinutes()
-      }
-    )
-    .update({
-      render_start_time: params.renderStartTime.format(MYSQL_TIME_FORMAT),
-      render_finish_time: finishTime.format(MYSQL_TIME_FORMAT),
-      render_time: duration.asMinutes()
-    })
-    .then(res => console.log("Updated", res));
+    db("graphs")
+        .where({ user_id: params.user * 1, unique_id: params.unique_id }, {
+            render_finish_time: finishTime.format(MYSQL_TIME_FORMAT),
+            render_time: duration.asMinutes()
+        })
+        .update({
+            render_start_time: params.renderStartTime.format(MYSQL_TIME_FORMAT),
+            render_finish_time: finishTime.format(MYSQL_TIME_FORMAT),
+            render_time: duration.asMinutes()
+        })
+        .then(res => console.log("Updated", res));
 }
 
 export function sendMail(to, subject, text) {
-  transporter.sendMail(
-    {
-      from: "Chartlapse <mailer@chartlapse.com>",
-      to,
-      subject,
-      text
-    },
-    (err, info) => {
-      console.log(1, err);
-      console.log(2, info.envelope);
-      console.log(3, info.messageId);
-    }
-  );
+    transporter.sendMail({
+            from: "Chartlapse <info@lorecore.com>",
+            to,
+            subject,
+            text
+        },
+        (err, info) => {
+            console.log(1, err);
+            console.log(2, info);
+            console.log(3, info);
+        }
+    );
 }
